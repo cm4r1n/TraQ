6a905ebc6f72e961af1d87f39af3ff7ee411ab0c
Carlos Marin <carlos.marinparra@gmail.com>
2024-08-29 02:38:45 -0600

    Mod/Sidebar Settings Link

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,30 +3,39 @@ import React from "react";
 function SideBar() {
   return(
     <div className='sidebar'>
-      <a className='sb-link'>
-        <i className='nf nf-md-lightning_bolt' />
-        <p className="sb-option">New note</p>
-      </a>
-      <a className='sb-link'>
-        <i className='nf nf-md-bell' />
-        <p className="sb-option">Notifications</p>
-      </a>
-      <a className='sb-link'>
-        <i className='nf nf-md-lead_pencil' />
-        <p className="sb-option">Modify</p>
-      </a>
-      <a className='sb-link'>
-        <i className='nf nf-md-delete' />
-        <p className="sb-option">Delete</p>
-      </a>
-      <a className='sb-link'>
-        <i className='nf nf-md-archive_check' />
-        <p className="sb-option">Archive</p>
-      </a>
-      <a className='sb-link'>
-        <i className='nf nf-md-notebook' />
-        <p className="sb-option">Add to book</p>
-      </a>
+      <div className='sb-menu'>
+        <a className='sb-link'>
+          <i className='nf nf-md-lightning_bolt' />
+          <p className="sb-option">New note</p>
+        </a>
+        <a className='sb-link'>
+          <i className='nf nf-md-bell' />
+          <p className="sb-option">Notifications</p>
+        </a>
+        <a className='sb-link'>
+          <i className='nf nf-md-lead_pencil' />
+          <p className="sb-option">Modify</p>
+        </a>
+        <a className='sb-link'>
+          <i className='nf nf-md-delete' />
+          <p className="sb-option">Delete</p>
+        </a>
+        <a className='sb-link'>
+          <i className='nf nf-md-archive_check' />
+          <p className="sb-option">Archive</p>
+        </a>
+        <a className='sb-link'>
+          <i className='nf nf-md-notebook' />
+          <p className="sb-option">Add to book</p>
+        </a>
+      </div>
+
+      <div className='sb-settings'>
+        <a className='sb-link'>
+          <i className='nf nf-fa-gear' />
+          <p className="sb-option">Settings</p>
+        </a>
+      </div>
     </div>
   );
 }
